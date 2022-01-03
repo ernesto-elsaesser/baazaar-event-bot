0c867e46ecaab19ba3a41b4d6308c43e1bdae293
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2022-01-03 15:19:19 +0100

    Filter D1 events

--- listen.py
+++ listen.py
@@ -15,12 +15,12 @@ moralis_url = f"wss://speedy-nodes-nyc.moralis.io/{moralis_token}/polygon/mainne
 provider = Web3.WebsocketProvider(moralis_url)
 web3 = Web3(provider)
 
-with open("aavegotchi_abi.json") as abi_file:
+with open("abi_aavegotchi.json") as abi_file:
     aavegotchi_abi = json.load(abi_file)
 
 aavegotchi_contract = web3.eth.contract(address='0x86935F11C86623deC8a25696E1C19a8659CbF95d', abi=aavegotchi_abi)
 
-with open("realm_abi.json") as abi_file:
+with open("abi_realm.json") as abi_file:
     realm_abi = json.load(abi_file)
 
 realm_contract = web3.eth.contract(address='0x1D0360BaC7299C86Ec8E99d0c1C9A95FEfaF2a11', abi=realm_abi)
@@ -42,17 +42,18 @@ def handle(action, tokenId, price_in_wei):
     price = price_in_wei / 1e18
 
     info = realm_contract.functions.getParcelInfo(tokenId).call()
+    district = info[6]
     address = info[1]
     size_id = info[5]
-    district = info[6]
     boosts = info[7]
     size = sizes[size_id]
 
     msg = f"{action} {size} D{district} {boosts} for {price:.0f} GHST - {address}"
     print(msg)
 
-    send_url = f"https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={msg}"
-    requests.get(send_url)
+    if district == 1:
+        send_url = f"https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={msg}"
+        requests.get(send_url)
 
 
 while True:
